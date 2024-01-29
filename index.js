5619be1a9702882edcc67ed8f283ef96c89e5def
edanielistvan <edanielistvan@gmail.com>
2024-01-29 08:28:27 +0100

    Modal setter

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ var in_max = document.querySelector("#in_max");
 var btn_add = document.querySelector("#btn_add");
 var color = document.querySelector("#color");
 var out_res = document.querySelector("#out_res");
-var modal = undefined;
+
+var modal = document.querySelector("#modal_container");
+var modal_title = document.querySelector("#modal_title");
+var change_val = document.querySelector("#change_val");
+var btn_plus = document.querySelector("#btn_plus");
+var btn_minus = document.querySelector("#btn_minus");
+var btn_set = document.querySelector("#btn_set");
 
 var content =  document.querySelector("#content");
 
@@ -15,6 +21,9 @@ types = ["num", "bar"];
 clrs = { p: "purple", r: "red", b: "blue", g: "green" };
 dat = [];
 id = 0;
+
+changing = false;
+change_id = undefined;
 
 function init(_) {
     in_type.innerHTML = "";
@@ -28,13 +37,19 @@ function init(_) {
     color.disabled = true;
 
     btn_add.addEventListener("click",addClick,null);
+    btn_plus.addEventListener("click",plusChange, null);
+    btn_minus.addEventListener("click",minusChange, null);
+    btn_set.addEventListener("click",setChange, null);
 
     in_name.addEventListener("keypress",no_num,null);
     in_value.addEventListener("keypress",no_text,null);
     in_min.addEventListener("keypress",no_text,null);
     in_max.addEventListener("keypress",no_text,null);
+    change_val.addEventListener("keypress",no_text,null);
 
     in_type.addEventListener("change",autofill,null);
+
+    modal.setAttribute("style","visibility: hidden !important;");
 }
 
 function capitalize(word) {
@@ -85,7 +100,6 @@ function addClick(_) {
     }
 
     var type = in_type.value;
-    var clr = color.value;
     var val = parseInt(in_value.value);
     var min = parseInt(in_min.value);
     var max = parseInt(in_max.value);
@@ -122,48 +136,92 @@ function isNum(str) {
     return false;
 }
 
-function containerClicked(e) {
-    var src = e.target;
+function containerClicked(id) {
+    console.log(id);
+    console.log(changing);
 
-    while (src.id === "") {
-        src = src.parentElement;
-    }
-
-    if (isNum(src.id)) {
-        var num = parseInt(src.id);
-        var data = dat.find(d => d.id == num);
-
+    if (!changing) {
+        var data = dat.find(d => d.id == id);
+        
         if (data != undefined) {
-           var value = window.prompt("Changing value of '" + data.name + "' (" + data.min + " - " + data.max + ") by:");
+            changing = true;
+            change_id = id;
 
-           if (isNum(value) && data.value + parseInt(value) <= data.max && data.value + parseInt(value) >= data.min) {
-                data.value += parseInt(value);
-                //updateRes();
-           }
+            modal_title.innerHTML = `Changing '${data.name}' (${data.min} - ${data.max})`;
+            modal.setAttribute("style","visibility: visible !important;");
+        }
+    }
+    else {
+        var data = dat.find(d => d.id == id);
+        
+        if (data != undefined) {
+            change_id = id;
+
+            modal_title.innerHTML = `Changing '${data.name}' (${data.min} - ${data.max})`;
         }
     }
 }
 
-function setupContainer(id) {
-  var container = document.createElement("div");
-  container.classList = "rpgui-container framed-golden rpgui-draggable";
-  container.setAttribute("data-rpguitype", "draggable");
-  container.draggable = false;
-  container.addEventListener("click", containerClicked, null);
-  container.id = "container_" + id;
-  return container;
+function plusChange(_) {
+    if (changing) {
+        var data = dat.find(d => d.id == change_id);
+        var next = data.value + parseInt(change_val.value);
+
+        if (next >= data.min && next <= data.max) {
+            data.value = next;
+        }
+
+        updateContainer(change_id);
+    }
 }
 
-function setupHeader(level, text) {
-  var header = document.createElement("h" + level);
-  header.innerHTML = text;
-  
-  return header;
+function minusChange(_) {
+    if (changing) {
+        var data = dat.find(d => d.id == change_id);
+        var next = data.value - parseInt(change_val.value);
+
+        if (next >= data.min && next <= data.max) {
+            data.value = next;
+        }
+
+        updateContainer(change_id);
+    }
 }
 
-function setupElem(type) {
-  return document.createElement(type);
+function setChange(_) {
+    if (changing) {
+        var data = dat.find(d => d.id == change_id);
+        var next = parseInt(change_val.value);
+
+        if (next >= data.min && next <= data.max) {
+            data.value = next;
+        }
+
+        updateContainer(change_id);
+    }
 }
+
+function updateContainer(id) {
+    if (changing) {
+        changing = false;
+        change_id = undefined;
+        modal.setAttribute("style","visibility: hidden !important;");
+    }
+    
+    var data = dat.find(d => d.id == id);
+    var val = document.querySelector(`#value_${id}`);
+
+    var type = data.type.split("-")[0];
+
+    if (type === "num") {
+        val.innerHTML = data.value;
+    }
+    else if (type === "bar") {
+        RPGUI.set_value(val,(data.value-data.min)/(data.max-data.min));  
+    }
+}
+
+
 
 function createContainer(id) {
     var data = dat.find(d => d.id == id);
@@ -176,13 +234,17 @@ function createContainer(id) {
         var title = setupHeader(1, `${data.name} (${data.min} - ${data.max})`);
 
         var clicker = setupElem("div");
-        clicker.id = data.id;
+        clicker.id = "click_"+data.id;
+        clicker.addEventListener("click", function() {
+            containerClicked(data.id);
+        }, null);
 
-        container.appendChild(title);
+        clicker.appendChild(title);
         container.appendChild(clicker);
 
         if (type[0] === "num") {
             var html = setupElem("p");
+            html.id = "value_" + data.id;
             html.innerHTML = data.value;
 
             clicker.appendChild(html);
@@ -190,6 +252,7 @@ function createContainer(id) {
         }
         else if (type[0] === "bar") {
             var html = setupElem("div");
+            html.id = "value_" + data.id;
             var clr = type[1];
 
             html.classList.add(clrs[clr]);
@@ -204,6 +267,27 @@ function createContainer(id) {
 
         RPGUI.create(container, "draggable");
     }
+}
+
+function setupContainer(id) {
+    var container = document.createElement("div");
+    container.classList = "rpgui-container framed-golden rpgui-draggable";
+    container.setAttribute("data-rpguitype", "draggable");
+    container.draggable = false;
+    container.addEventListener("click", containerClicked, null);
+    container.id = "container_" + id;
+    return container;
+}
+
+function setupHeader(level, text) {
+    var header = document.createElement("h" + level);
+    header.innerHTML = text;
+
+    return header;
+}
+
+function setupElem(type) {
+    return document.createElement(type);
 }
 
 window.addEventListener("load",init,null);
